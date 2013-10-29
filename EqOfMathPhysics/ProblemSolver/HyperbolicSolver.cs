c5847b49dc439ef34f35ad22156c9755834a262d
Stanislav <stasyan93@bk.ru>
2013-10-29 17:23:46 +0400

    Hyper

--- EqOfMathPhysics/ProblemSolver/HyperbolicSolver.cs
+++ EqOfMathPhysics/ProblemSolver/HyperbolicSolver.cs
@@ -2,9 +2,16 @@
 {
     class HyperbolicSolver : ISolver
     {
+        private readonly HyperbolicProblem _problem;
+
+        public HyperbolicSolver(HyperbolicProblem problem)
+        {
+            _problem = problem;
+        }
+
         public ProblemResult Solve(IProblem problem)
         {
-            
+            return null;
         }
     }
 }
