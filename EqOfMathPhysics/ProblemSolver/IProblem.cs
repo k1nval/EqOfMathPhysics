8c55d62d374722557097c4749fd2ec96c466962f
Stanislav <stasyan93@bk.ru>
2013-10-29 17:15:46 +0400

    delete unused

--- EqOfMathPhysics/ProblemSolver/IProblem.cs
+++ EqOfMathPhysics/ProblemSolver/IProblem.cs
@@ -1,11 +1,5 @@
 ﻿namespace ProblemSolver
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
     public interface IProblem
     {
         double H { get; set; }
