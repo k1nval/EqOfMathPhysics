bf2c51fad539abd25a3eb6f4fb3c85e2d57f7d6c
Ayrat <jastis1993@yandex.ru>
2013-10-30 22:41:29 +0400

    ...

--- EqOfMathPhysics/ProblemSolver/Solvers/EllipticSolver.cs
+++ EqOfMathPhysics/ProblemSolver/Solvers/EllipticSolver.cs
@@ -25,44 +25,40 @@
 
         public Layer Solve(int needLayer)
         {
-            var firstLayer = new Layer(_nx);
-            var secondLayer = new Layer(_nx);
-            var thirdLayer = new Layer(_nx);
+            var lastLayer = this.PrepareLayer(_ny);
 
-            var lastLayer = PrepareLayer(_nx);
-
-            for (int i = 1; i < _nx - 1; i++)
+            for (int i = 1; i < _ny - 1; i++)
             {
-                firstLayer[i] = _problem.fi(i * _problem.h, 0);
+                lastLayer[i] = _problem.fi(i * _problem.h, _ny - 1);
             }
 
-            for (int i = 1; i < _nx; i++)
-            {
-                lastLayer[i] = _problem.fi(i * _problem.h, _ny);
-            }
-
+            var matrix = new double[_nx + 1, _ny];
+            var k = 0;
             for (int j = 1; j < _ny - 1; j++)
             {
-                firstLayer = this.PrepareLayer(j - 1);
-                secondLayer = this.PrepareLayer(j);
-                thirdLayer = this.PrepareLayer(j + 1);
-
                 for (int i = 1; i < _nx - 1; i++)
                 {
-                    var arr = new double[5];
-                    arr[0] = i + 1 == _nx - 1 ? secondLayer[_nx - 1] : 1; // 1
-                    arr[1] = i - 1 == 0 ? secondLayer[0] : 1; // 0
-                    arr[2] = j + 1 == _ny - 1 ? thirdLayer[i] : 1; // 1
-                    arr[3] = j - 1 == 0 ? firstLayer[i] : 1; // 0.866
-                    arr[4] = -4;
-
-                    var d = arr;
+                    matrix[k, 0] = i + 1 == _nx - 1 ? _problem.fi(_nx - 1, j * _problem.h) : 1;
+                    matrix[k, 1] = i - 1 == 0 ? _problem.fi(0, j * _problem.h) : 1;
+                    matrix[k, 2] = j + 1 == _ny - 1 ? _problem.fi(i * _problem.h, _ny - 1) : 1;
+                    matrix[k, 3] = j - 1 == 0 ? _problem.fi(i * _problem.h, 0) : 1;
+                    matrix[k, 4] = -4.00;
+                    k++;
                 }
             }
 
-            var result = new Layer((_nx - 1) * (_ny - 1)) { Number = needLayer };
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    var elem = matrix[i, j] < 0.001 && matrix[i, j] > -0.0001 ? 0.00 : matrix[i, j];
+                    Console.Write("{0:F2} ", elem);
+                }
 
-            return result;
+                Console.WriteLine();
+            }
+
+            return default(Layer);
         }
 
         private Layer PrepareLayer(int number)
@@ -71,7 +67,17 @@
 
             newLayer[0] = _problem.fi(0, number * _problem.h);
 
-            newLayer[_nx - 1] = _problem.fi(_nx, number * _problem.h);
+            if (Math.Abs(newLayer[0]) < 0.001)
+            {
+                newLayer[0] = 0;
+            }
+
+            newLayer[_nx - 1] = _problem.fi(_nx - 1, number * _problem.h);
+
+            if (Math.Abs(newLayer[_nx - 1]) < 0.001)
+            {
+                newLayer[_nx - 1] = 0;
+            }
 
             return newLayer;
         }
